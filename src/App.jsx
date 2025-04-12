599f274093035243583ae371c747cd6e7e9e39ba
werliton <werlitoncarlos@gmail.com>
2025-04-12 17:19:28 -0300

    estrutura inicial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className='flex gap-2'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WS Boilerplate Vite + React + Vitest</h1>
+      
+    </div>
+  )
+}
+
+export default App
