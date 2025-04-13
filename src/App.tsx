d23d3ae02dc0f54e2e9feaa1e09e99b28855e101
werliton <werlitoncarlos@gmail.com>
2025-04-12 22:32:20 -0300

    refact: substituindo useState por userReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { useReducer } from "react";
 import Search from "./components/Search";
 import Records from "./components/Records";
 import { medical_records } from "./utilities";
 
 const title = "Patient Medical Records";
 
-const App = () => {
-  const [selectedPatient, setSelectedPatient] = React.useState("");
-  const [dataTable, setDataTable] = React.useState([]);
+type ACTION_TYPE = {
+  type: "SET_PATIENT" | "SET_DATA";
+  payload: any;
+};
 
-  const handlePatient = (value: any) => {
-    setSelectedPatient(value);
+const initialState = {
+  patientId: "",
+  data: [] as any,
+};
+
+function reducer(state = initialState, action: ACTION_TYPE) {
+  switch (action.type) {
+    case "SET_PATIENT":
+      return {
+        ...state,
+        patientId: action.payload,
+      };
+
+    case "SET_DATA":
+      return {
+        ...state,
+        data: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { patientId, data } = state;
+
+  const handlePatient = (value: string) => {
+    dispatch({
+      type: "SET_PATIENT",
+      payload: value,
+    });
   };
 
   const setRecord = () => {
     // buscar no array de data o dados do usuario e enviar para a tabela
-    if (selectedPatient) {
+    if (patientId) {
       const foundPatient = medical_records.find(
-        (item) => item.id === selectedPatient
+        (item) => item.id === patientId
       );
 
       if (foundPatient?.id) {
-        setDataTable(foundPatient.data as any);
+        dispatch({
+          type: "SET_DATA",
+          payload: foundPatient.data as any,
+        });
       }
     }
   };
@@ -29,13 +64,19 @@ const App = () => {
   const setNextRecord = () => {
     // encontrar pelo index e definir o proximo index
     const currentIndex = medical_records.findIndex(
-      (item) => item.id === selectedPatient
+      (item) => item.id === patientId
     );
     const nextIndex = (currentIndex + 1) % medical_records.length;
     const nextPatient = medical_records[nextIndex];
 
-    setDataTable(nextPatient.data as any);
-    setSelectedPatient(nextPatient.id);
+    dispatch({
+      type: "SET_PATIENT",
+      payload: nextPatient.id,
+    });
+    dispatch({
+      type: "SET_DATA",
+      payload: nextPatient.data as any,
+    });
   };
 
   return (
@@ -47,10 +88,10 @@ const App = () => {
         <Search
           setRecord={setRecord}
           setId={handlePatient}
-          selectedPatientId={selectedPatient}
+          selectedPatientId={patientId}
         />
-        {dataTable.length > 0 && (
-          <Records data={dataTable} setNextRecord={setNextRecord} />
+        {data.length > 0 && (
+          <Records data={data} setNextRecord={setNextRecord} />
         )}
       </div>
     </div>
