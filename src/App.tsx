5e17c3bb53e8ed2fd010425edf30677083e7cd1c
werliton <werlitoncarlos@gmail.com>
2025-04-26 10:09:17 -0300

    feat(Singleton): implementing Singleton Pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import Search from "./components/Search";
 import Records from "./components/Records";
 import { usePatient } from "./hooks/usePatient";
-
-const title = "Patient Medical Records";
+import { Header } from "./components/Header";
 
 const App = () => {
   const { data, handlePatient, patientId, setNextRecord, setRecord } =
     usePatient();
 
   return (
-    <div className="bg-white h-screen flex flex-col">
-      <nav className="h-14 bg-black text-green-500 flex items-center p-2 font-black">
-        {title}
-      </nav>
-      <div className="flex flex-col items-center w-full">
+    <div className="flex h-screen flex-col bg-white">
+      <Header />
+      <div className="flex w-full flex-col items-center">
         <Search
           setRecord={setRecord}
           setId={handlePatient}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+// Design Patterns: Singleton
+// https://deviq.com/design-patterns/singleton
+export const Header = () => {
+  return (
+    <nav className="flex h-14 items-center bg-black p-2 font-black text-green-500">
+      WS Medical Records
+    </nav>
+  );
+};
