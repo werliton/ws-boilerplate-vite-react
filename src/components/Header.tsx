82f8426b3f42d7e6684803f0ced02adf0fee8b82
werliton <werlitoncarlos@gmail.com>
2025-04-26 22:00:08 -0300

    chore: improving singleton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 // Design Patterns: Singleton
+
+import React from "react";
+
 // https://deviq.com/design-patterns/singleton
-export const Header = () => {
+export const Header = React.memo(() => {
   return (
     <nav className="flex h-14 items-center bg-black p-2 font-black text-green-500">
       WS Medical Records
     </nav>
   );
-};
+});
