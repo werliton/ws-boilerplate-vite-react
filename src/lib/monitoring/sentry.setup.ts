f385c66207a574403d3854ea656b23f5c7b4710b
werliton <werlitoncarlos@gmail.com>
2025-05-01 14:30:43 -0300

    feat: setting up sentry

---
+++ src/lib/monitoring/sentry.setup.ts
@@ -0,0 +1,28 @@
+import * as Sentry from "@sentry/react";
+
+declare global {
+  interface ImportMeta {
+    env: {
+      VITE_SENTRY_DSN: string;
+    };
+  }
+}
+const _meta: ImportMeta = import.meta
+
+Sentry.init({
+  dsn: _meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import "./lib/monitoring/sentry.setup";
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
