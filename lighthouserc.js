6ffb3a9d7267a02f5b417db339b7a7ebbf901eb5
werliton <werlitoncarlos@gmail.com>
2025-05-02 23:33:21 -0300

    chore: add collect to lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,12 @@
 export default {
     ci: {
+        collect: {
+            startServerCommand: 'pnpm run preview',
+            startServerReadyPattern: 'Local:', // regex que ajuda o LHCI saber quando o servidor está pronto
+            url: ['http://localhost:4173/'],
+          },
         upload: {
-        target: 'temporary-public-storage',
+            target: 'temporary-public-storage',
         },
     },
 };
