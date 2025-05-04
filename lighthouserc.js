e8a316ec8ff7a3866a71ed0655257b42c7e31a5b
werliton <werlitoncarlos@gmail.com>
2025-05-03 23:20:27 -0300

    feat: setting up playwright to e2e tests

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,20 +3,13 @@ export default {
         collect: {
             url: ['http://localhost:4173/'],
             startServerCommand: 'pnpm run preview',
-            startServerReadyPattern: 'Local:',
-            settings: {skipAudits: ['canonical', 'uses-http2', 'uses-long-cache-ttl']}
+            startServerReadyPattern: 'Local:'
         },
         upload: {
             target: 'temporary-public-storage',
         },
         assert: {
-            preset: 'lighthouse:no-pwa',
-            assertions: {
-              'meta-description': 'off',
-              'select-name': 'off',
-              'unused-javascript': 'off',
-              'render-blocking-resources': 'off'
-            },
+            preset: 'lighthouse:recommended'
           },
     },
 };
